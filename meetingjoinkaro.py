87f8c827c8238089385d0f0cd0346ddf416db0d8
aana123 <aanapandey05@gmail.com>
2021-10-31 15:21:55 +0530

    added meet link

--- meetingjoinkaro.py
+++ meetingjoinkaro.py
@@ -1,10 +1,13 @@
 import os
+import time
 from selenium import webdriver
 
 os.environ['PATH'] += r"C:\Users\apoor\Downloads\chromedriver_win32"
 driver = webdriver.Chrome()
-driver.get("https://www.duckduckgo.com/")
+driver.get("https://meet.google.com/hvy-odeb-oet")
 
-search_box = driver.find_element_by_id("search_form_input_homepage")
-search_box.send_keys("Meeting Join Karo")
-search_box.submit()
+time.sleep(3000)
+
+# search_box = driver.find_element_by_id("i3")
+# search_box.send_keys("Meeting Join Karo")
+# search_box.submit()
